688ad55a1184180b1d621784cce759eb1b60e48e
Fabian Gotzen <fabian.gotzen@gmail.com>
2023-10-30 21:43:19 +0100

    Initial Setup

--- src/test/java/com/fago/testcontainers/TestcontainersInitializer.java
+++ src/test/java/com/fago/testcontainers/TestcontainersInitializer.java
@@ -3,6 +3,7 @@ package com.fago.testcontainers;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,18 +11,16 @@ import org.testcontainers.containers.PostgreSQLContainer;
 class TestcontainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     static final KafkaContainer KAFKA_CONTAINER;
-    static final PostgreSQLContainer POSTGRES_CONTAINER;
+    static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
 
     static {
         KAFKA_CONTAINER = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
-        POSTGRES_CONTAINER = new PostgreSQLContainer(DockerImageName.parse("postgres:12"));
+        POSTGRES_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:12"));
     }
 
     @Override
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-        // testcontainers deepstart method
-        KAFKA_CONTAINER.start();
-        POSTGRES_CONTAINER.start();
+        Startables.deepStart(KAFKA_CONTAINER, POSTGRES_CONTAINER).join();
 
         TestPropertyValues.of(
                 "spring.kafka.bootstrap-servers=" + KAFKA_CONTAINER.getBootstrapServers(),
